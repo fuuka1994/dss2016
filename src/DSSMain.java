7e37f5bff16ffd65fe102e6c61b474af56b34897
fuuka1994 <ngantn1994gmail.com>
2016-01-04 22:43:48 +0700

    Commit after try to resolve conflicts.

--- src/DSSMain.java
+++ src/DSSMain.java
@@ -2,12 +2,7 @@
 public class DSSMain {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("Hello World!");
-=======
 		System.out.println("Anh Đan Sấm Sét bá chủ thế giới!");
->>>>>>> branch 'master' of https://github.com/fuuka1994/dss2016
-
 	}
 
 }
