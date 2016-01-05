a518a54cb4b3ceef796b738d8cc35e7f336a098e
fuuka1994 <ngantn1994@gmail.com>
2016-01-05 17:02:46 +0700

    change view size

--- src/view/ChatWindow.java
+++ src/view/ChatWindow.java
@@ -15,7 +15,7 @@ public class ChatWindow extends JFrame {
 	public ChatWindow(){
 		this.setResizable(false);
 		this.setTitle("H%u1EC7%20th%u1ED1ng%20tr%u1EE3%20gi%FAp%20quy%u1EBFt%20%u0111%u1ECBnh%20v%u1EC1%20Y%20h%u1ECDc%20c%u1ED5%20truy%u1EC1n");
-		this.setBounds(150, 5, 1000, 710);
+		this.setBounds((java.awt.Toolkit.getDefaultToolkit().getScreenSize().width-600)/2, 5, 600, 600);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setLayout(new FlowLayout());
 	}
